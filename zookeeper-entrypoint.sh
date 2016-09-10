c18adea11c440bdc5f8fe1e69c6d57be04141c85
Shreyas Kulkarni <shyran@gmail.com>
2016-09-10 15:43:44 +0530

    initial dockerfile and other helper files

---
+++ zookeeper-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ZK_HOME=/opt/zookeeper
+
+echo $ZK_NODE_ID >$ZK_NODE_ID/data/myid
+$ZK_HOME/conf-helpers/gen.zoo.cfg >$ZK_HOME/conf/zoo.cfg
+
+trap "kill -15 -1" EXIT KILL
+
+exec "$ZK_HOME/bin/zkServer.sh start-foreground conf/zoo.cfg"
+
