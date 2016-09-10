f57bb5162fe784fb4174f08685a78df527789966
Shreyas Kulkarni <shyran@gmail.com>
2016-09-10 16:57:32 +0530

    fixed the entrypoint

--- zookeeper-entrypoint.sh
+++ zookeeper-entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 ZK_HOME=/opt/zookeeper
 
-echo $ZK_NODE_ID >$ZK_NODE_ID/data/myid
+set -e
+
+echo $ZK_NODE_ID >$ZK_HOME/data/myid
 $ZK_HOME/conf-helpers/gen.zoo.cfg >$ZK_HOME/conf/zoo.cfg
 
 trap "kill -15 -1" EXIT KILL
 
-exec "$ZK_HOME/bin/zkServer.sh start-foreground $ZK_HOME/conf/zoo.cfg"
+$ZK_HOME/bin/zkServer.sh start-foreground
 
